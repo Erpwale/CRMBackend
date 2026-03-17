e446842f04bab6b711f96cd696adae9f349332b1
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-03-17 15:50:19 +0530

    remove error

--- models/callBooking.js
+++ models/callBooking.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const calbooking = new mongoose.Schema(
+const callBooking = new mongoose.Schema(
   {
     companyId: {
       type: mongoose.Schema.Types.ObjectId,
