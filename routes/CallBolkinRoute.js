bac72aabbeff931db723f56f725604c9221dd005
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-03-17 15:53:42 +0530

    added new

--- routes/CallBolkinRoute.js
+++ routes/CallBolkinRoute.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Support = require("../models/callBooking");
-const authMiddleware = require("../middleware/auth");
-
+const { authMiddleware, adminOnly } = require("../middleware/auth");
 
 // ✅ CREATE
 router.post("/create", authMiddleware, async (req, res) => {
