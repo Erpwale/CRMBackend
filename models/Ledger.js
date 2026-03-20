12f21d823b459d08892c463ad64c8c5179ff6e7d
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-03-20 15:29:56 +0530

    sds

--- models/Ledger.js
+++ models/Ledger.js
@@ -1,40 +1,35 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const ledgerSchema = new mongoose.Schema(
   {
     companyId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Company", // optional
+      ref: "Company",
       required: true,
       index: true,
     },
-
     companyName: {
       type: String,
       required: true,
       trim: true,
     },
-
     gstin: {
       type: String,
       required: true,
       uppercase: true,
       trim: true,
     },
-
     pan: {
       type: String,
       required: true,
       uppercase: true,
       trim: true,
     },
-
     tan: {
       type: String,
       uppercase: true,
       trim: true,
     },
-
     msme: {
       type: String,
       uppercase: true,
