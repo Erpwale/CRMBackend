5c191a347aa9b8f4b3ae484b1db8be11083de43f
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-03-23 16:38:10 +0530

    ooo

--- routes/PostOffice.js
+++ routes/PostOffice.js
@@ -11,22 +11,36 @@ const upload = multer({ dest: "uploads/" });
 
 // ✅ CSV Upload API
 router.get("/import-csv", async (req, res) => {
-  const results = [];
+  let batch = [];
+  const BATCH_SIZE = 1000;
 
-  fs.createReadStream("data/pincode.csv") // 👈 direct path
+  fs.createReadStream("data/pincode.csv")
     .pipe(csv())
-    .on("data", (data) => {
-      results.push({
+    .on("data", async (data) => {
+      batch.push({
         state: data.statename,
         district: data.district,
         city: data.officename,
         pincode: Number(data.pincode),
       });
+
+      // Insert batch
+      if (batch.length === BATCH_SIZE) {
+        try {
+          await Location.insertMany(batch, { ordered: false });
+          batch = [];
+        } catch (err) {
+          console.log("Batch error:", err.message);
+        }
+      }
     })
     .on("end", async () => {
       try {
-        await Location.insertMany(results);
-        res.json({ message: "✅ CSV imported successfully" });
+        if (batch.length > 0) {
+          await Location.insertMany(batch, { ordered: false });
+        }
+
+        res.json({ message: "✅ Filtered 50k data imported successfully" });
       } catch (err) {
         res.status(500).json({ error: err.message });
       }
