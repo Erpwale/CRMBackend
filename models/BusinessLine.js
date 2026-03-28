8e724d593e0ccc6bf8b8f823650097d21513ddf9
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-03-28 14:45:27 +0530

    added discount

--- models/BusinessLine.js
+++ models/BusinessLine.js
@@ -6,19 +6,19 @@ const ProductSchema = new mongoose.Schema({
   rate: { type: Number, default: 0 },
   gst: { type: Number, default: 0 },
   discount: { type: Number, default: 0 },
+
+  // ✅ move here
+  discountAllowed: { type: Boolean, default: false },
 });
 
 const BusinessLineSchema = new mongoose.Schema(
   {
     businessLine: { type: String, required: true },
-    discountAllowed: { type: Boolean, default: false },
+
+    // ❌ remove from here
+    // discountAllowed: { type: Boolean },
 
     products: [ProductSchema],
-
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
   },
   { timestamps: true }
 );
