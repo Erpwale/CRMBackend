03a46b03e894e69b001ae94bf11aba6ed6784531
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-07 16:26:56 +0530

    fsdasf

--- utils/generateProposalPDF.js
+++ utils/generateProposalPDF.js
@@ -47,45 +47,7 @@ const footerBase64 = fs.readFileSync(
       .join("")}
   </ol>
 `;
-headerTemplate: `
-  <div style="width:100%; text-align:center;">
-    <img src="data:image/jpeg;base64,${headerBase64}" style="width:100%;"/>
-  </div>
-`,
-footerTemplate: `
-  <div style="width:100%; font-size:10px; padding:0 20px;">
 
-    <div style="display:flex; justify-content:space-between;">
-      <div>
-        <b>For, MS ERPWale Pvt. Ltd.</b>
-      </div>
-
-      <div style="text-align:right;">
-        <b>For, ${data.companyName}<br/>${data.contactName}</b>
-      </div>
-    </div>
-
-    <div style="margin-top:5px;">
-      <b>Regards,</b><br/>
-      ${data.userName || ""}<br/>
-      ${data.email || ""}<br/>
-      ${data.mobile || ""}
-    </div>
-
-    <div style="text-align:center; margin-top:5px;">
-      (Computer Generated Document so Signature not required)
-    </div>
-
-    <div style="margin-top:5px;">
-      <img src="data:image/jpeg;base64,${footerBase64}" style="width:100%;" />
-    </div>
-
-    <div style="text-align:right; font-size:9px;">
-      Page <span class="pageNumber"></span> of <span class="totalPages"></span>
-    </div>
-
-  </div>
-`
 console.log("termsHTML:", termsHTML);
     // ✅ HTML TEMPLATE (YOUR FULL TEMPLATE HERE)
     const html = `
