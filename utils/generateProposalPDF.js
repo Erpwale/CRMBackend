57cfd69e50f2489d3dc7cda22e54b5fbcb29c154
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-09 17:14:36 +0530

    sd

--- utils/generateProposalPDF.js
+++ utils/generateProposalPDF.js
@@ -270,8 +270,8 @@ const emptyRows = Array.from({
   displayHeaderFooter: true,
 
   margin: {
-    top: "180px",
-    bottom: "180px"
+    top: "90px",
+    bottom: "100px"
   },
 
   headerTemplate: `
