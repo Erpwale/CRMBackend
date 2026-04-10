6e9e2060907813acdd4f9b23037578a39959f2c7
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-10 17:11:27 +0530

    okk

--- utils/generateProposalPDF.js
+++ utils/generateProposalPDF.js
@@ -88,7 +88,7 @@ const emptyRows = Array.from({
   length: Math.max(0, maxRows - data.products.length)
 }).map((_, i) => `
 <tr>
-  <td>${data.products.length + i + 1}</td>
+  <td></td>
   <td>&nbsp;</td>
   <td></td>
   <td></td>
@@ -155,7 +155,7 @@ const emptyRows = Array.from({
 
 
 <h2 style="text-align:center;">BUSINESS PROPOSAL</h2>
-<div style="display: flex;">
+<div style="display: flex;justify-content: space-between;">
 
 <div>
   <b>To:</b><br/>
