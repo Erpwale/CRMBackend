97d363fac23be809383edf14aa77b89b3f8dda9b
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-13 10:38:47 +0530

    added image size

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // ✅ Create HTTP server
 const server = http.createServer(app);
 
@@ -21,7 +22,10 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-
+if (JSON.stringify(payload).length > 1000000) {
+  toast.error("Data too large ❌ Reduce content/images");
+  return;
+}
 // ✅ Socket connection
 io.on("connection", (socket) => {
   console.log("✅ User connected:", socket.id);
