f7c8e53fce6d28ad746baab29de5c2db6203f2a7
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-13 15:46:09 +0530

    sd

--- models/Proposall.js
+++ models/Proposall.js
@@ -13,7 +13,7 @@ const proposallSchema = new mongoose.Schema(
     },
       user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
+      ref: "User",
       required: true,
     },
 
