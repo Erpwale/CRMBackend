d1391fd8c384f47a3466ba73806516fef868e63e
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-15 23:53:59 +0530

    added 13 Px

--- utils/generateProposalPDF.js
+++ utils/generateProposalPDF.js
@@ -85,7 +85,7 @@ const productRows = data.products.map((p, i) => `
 <tr>
   <td class="center">${i + 1}</td>
    <td class="left ">${p.name || ""}</br>
-   <p style="font-size:10px"> ${p.description}</p></td>
+   <p style="font-size:13px"> ${p.description}</p></td>
   <td class="center">${p.qty || ""}</td>
   <td class="right">${p.rate || ""}.00</td>
   <td class="right">${p.totalValue || ""}.00</td>
