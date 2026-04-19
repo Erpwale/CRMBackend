9dfd706eecec059e2a5b6e71dcd1f0cc9a2a89c0
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-19 12:46:42 +0530

    added 8px in br tag

--- utils/generateProposalPDF.js
+++ utils/generateProposalPDF.js
@@ -82,7 +82,7 @@ const formatAddress = (address) => {
   return `${line1}<br/>${line2}`;
 };
 const productRows = data.products.map((p, i) => {
-  const desc = (p.description || "").replace(/\n/g, '<br style="display:block; margin-top:6px;" />'); // ✅ FIX
+  const desc = (p.description || "").replace(/\n/g, '<br style="display:block; margin-top:8px;" />'); // ✅ FIX
 
   return `
 <tr>
