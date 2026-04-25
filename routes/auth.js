798d69363dcd0b97f9406fb01c043e4aa2ed3f31
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-04-25 14:52:29 +0530

    added me api

--- routes/auth.js
+++ routes/auth.js
@@ -211,5 +211,17 @@ router.get("/users", authMiddleware, async (req, res) => {
     res.status(500).json({ message: "Error fetching users" });
   }
 });
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
 
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user" });
+  }
+});
 module.exports = router;
