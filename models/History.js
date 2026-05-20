bf398598c4c9133b390f07741a4ffb188c01fc27
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-05-20 14:41:44 +0530

    addded historySchema

--- models/History.js
+++ models/History.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const activitySchema = new mongoose.Schema({
+const historySchema = new mongoose.Schema({
 
   userId: {
     type: mongoose.Schema.Types.ObjectId,
