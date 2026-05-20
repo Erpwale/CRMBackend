1db51d19d48b0b67c3de773d47f946be6bdf875f
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-05-20 15:21:34 +0530

    fetch history

--- routes/auth.js
+++ routes/auth.js
@@ -697,5 +697,34 @@ console.log("Location:", geo);
   }
 );
 
+// Fetch History of user
 
+router.get(
+  "/activity/:userId",
+  authMiddleware,
+  async (req, res) => {
+
+    try {
+
+      const { userId } = req.params;
+
+      const activities = await history.find({
+        userId
+      })
+        .sort({ createdAt: -1 });
+
+      res.status(200).json(activities);
+
+    } catch (error) {
+
+      console.log(error);
+
+      res.status(500).json({
+        message: "Server Error"
+      });
+
+    }
+
+  }
+);
 module.exports = router;
