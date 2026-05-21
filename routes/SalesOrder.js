0e0b2c4abbd535795bd7c7acbc16f8a9eb25515f
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-05-21 14:26:58 +0530

    added 120px

--- routes/SalesOrder.js
+++ routes/SalesOrder.js
@@ -758,7 +758,7 @@ text-align: center
         .footer-left {
             width: 50%;
             padding: 8px;
-                margin-top: 70px;
+                margin-top: 120px;
 
             /* border-right: 0.5px solid #00000061; */
         }
