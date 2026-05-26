fbc193b5e269f66d24b19fad2c0f91dcd3f8ea3a
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-05-26 12:21:25 +0530

    added assignd to

--- routes/allticket.js
+++ routes/allticket.js
@@ -646,9 +646,36 @@ router.get("/support/:supportId", async (req, res) => {
 
     const tickets = await Ticket.find({
       assignedTo: supportId,
+      status: "Assigned",
     })
       .populate("assignedTo", "name email")
       .populate("customerId", "name email")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      success: true,
+      count: tickets.length,
+      data: tickets,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch support tickets",
+    });
+  }
+});
+router.get("/support/ticket/:supportId", async (req, res) => {
+  try {
+    const { supportId } = req.params;
+
+    const tickets = await Ticket.find({
+      assignedTo: supportId,
+    })
+      .populate("assignedTo", "name email")
+      .populate("customerId", "name email")
+      .populate("companyId", "companyName phone email")
       .sort({ createdAt: -1 });
 
     res.status(200).json({
