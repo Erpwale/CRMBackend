67030d315d37ee9f494d49acade3b2b5822b5db8
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-05-29 23:29:51 +0530

    added

--- models/TicketSchema.js
+++ models/TicketSchema.js
@@ -19,7 +19,11 @@ const ticketSchema = new mongoose.Schema(
     subCategory: String,
 
     description: String,
-
+ priority: {
+      type: String,
+      enum: ["Low", "Medium", "High", "Urgent"],
+      default: "Low",
+    },
     contactPerson: String,
 
     contactId: {
