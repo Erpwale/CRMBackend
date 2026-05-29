d887ef1f0b2017ec40bb2bade75c85b2b524c41f
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-05-29 22:14:22 +0530

    updated save

--- models/Contact.js
+++ models/Contact.js
@@ -60,16 +60,19 @@ const ContactSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-ContactSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+ContactSchema.pre("save", async function () {
+
+  if (!this.isModified("password")) {
+    return;
+  }
 
   const salt = await bcrypt.genSalt(10);
 
-  this.password = await bcrypt.hash(this.password, salt);
-
-  next();
+  this.password = await bcrypt.hash(
+    this.password,
+    salt
+  );
 });
-
 
 
 
