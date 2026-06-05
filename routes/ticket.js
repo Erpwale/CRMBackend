e69ca56cc03b9f4417e9c69cf6619a362a276ca5
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-06-05 20:45:03 +0530

    added month in string

--- routes/ticket.js
+++ routes/ticket.js
@@ -16,11 +16,11 @@ const generateTicketNumber = async () => {
 
   const yy = String(now.getFullYear()).slice(-2);
 
-  const mm = String(now.getMonth() + 1).padStart(2, "0");
+const mm = now.toLocaleString("en-US", { month: "short" });
 
   const dd = String(now.getDate()).padStart(2, "0");
 
-  const datePart = `${yy}${mm}${dd}`;
+  const datePart = `${dd}${mm}${yy}`;
 
   // random 4 digit
   const random = Math.floor(1000 + Math.random() * 9000);
