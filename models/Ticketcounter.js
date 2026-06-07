04f96d2a9083fa509524c313b7a51c141396c0e3
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-06-07 13:20:46 +0530

    okk

--- models/Ticketcounter.js
+++ models/Ticketcounter.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
 
 const ticketCounterSchema = new mongoose.Schema({
-  _id: String,
-  seq: {
-    type: Number,
-    default: 1999
-  }
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 1999 }
 });
 
-module.exports = mongoose.model(
-  "TicketCounter",
-  ticketCounterSchema
-);
+module.exports = mongoose.model("TicketCounter", ticketCounterSchema);
