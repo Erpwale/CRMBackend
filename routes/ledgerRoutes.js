5a2cdc1030d2c060b4475024bc345800d84d4706
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-06-07 23:25:22 +0530

    added

--- routes/ledgerRoutes.js
+++ routes/ledgerRoutes.js
@@ -131,7 +131,7 @@ const isValidGSTIN = (gstin, selectedState, pan) => {
 
   const gstRegex =
     /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][1-9A-Z]Z[0-9A-Z]$/;
-
+const gstTypeValue = gstType?.trim().toLowerCase();
 if (
   gstType !== "Unregistered" &&
   gstType !== "Consumer"
