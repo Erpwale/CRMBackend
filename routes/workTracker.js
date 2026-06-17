85e43d0f82d0b278cbedc3a8d7bdc602d91e3d2b
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-06-17 15:37:42 +0530

    addedTicketSchema

--- routes/workTracker.js
+++ routes/workTracker.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const WorkBench = require("../models/workTrackerSchema");
+const Ticket = require("../models/TicketSchema");
 
 
 const getTodayRecord = async (userId) => {
