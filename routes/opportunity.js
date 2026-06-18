8904644c00eb9c2b8f314fea2d630b644fbd490f
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-06-18 15:47:55 +0530

    added SalesOrder

--- routes/opportunity.js
+++ routes/opportunity.js
@@ -2,20 +2,46 @@
 const express = require("express");
 const router = express.Router();
 const Deal = require("../models/opportunityModel.js");
-
+const SalesOrder=require("../models/SalesOrder")
 // ➕ CREATE DEAL
 router.post("/add", async (req, res) => {
   try {
+    // Check Annual Support Cover
+    if (req.body.businessLine === "Annual Support Cover") {
+      const licenseNo =
+        req.body.products?.[0]?.amcDetails?.licenseNo;
+
+      if (licenseNo) {
+        const existingAMC = await SalesOrder.findOne({
+          businessLine: "Annual Support Cover",
+          "products.amcDetails.licenseNo": licenseNo,
+          isBill: true,
+          isOutstanding: false,
+        });
+
+        if (existingAMC) {
+          return res.status(400).json({
+            success: false,
+            message: `Annual Support Cover already exists for License No ${licenseNo}.`,
+          });
+        }
+      }
+    }
+
     const deal = new Deal(req.body);
     await deal.save();
 
     res.status(201).json({
+      success: true,
       message: "Opportunity created successfully",
       data: deal,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error creating deal" });
+    res.status(500).json({
+      success: false,
+      message: "Error creating deal",
+    });
   }
 });
 
