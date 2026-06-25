5c388aba6fd67ef3f48cc88f2cc76c23e1ac91ff
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-06-25 15:58:04 +0530

    axios

--- routes/ledgerRoutes.js
+++ routes/ledgerRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const Ledger = require("../models/Ledger");
-const Ledger = require("axios");
+const axios = require("axios");
 
 
 const router = express.Router();
