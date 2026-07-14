04f28c1d8fe410d44cc806a73467dc4497d6b378
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-07-14 13:04:44 +0530

    added authmidelware

--- routes/trainingRoutes.js
+++ routes/trainingRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-
+const { authMiddleware, adminOnly } = require("../middleware/auth");
 const Training = require("../models/Training");
 
 
