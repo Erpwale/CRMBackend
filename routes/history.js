27d9bc37b9b7f17ca34712a91049a5996786d7d9
DEEPALI More <62871080+deeps-19@users.noreply.github.com>
2026-07-21 12:00:08 +0530

    updated auth

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const History = require("../models/History");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/auth");
 
 
 // Get All History
